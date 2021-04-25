19a38960d56beb1a36d796307aea1450103009cf
skzhuzhuhappy <15201255789@163.com>
2021-04-25 16:48:26 +0800

    初始化众筹模型

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,14 @@ use Illuminate\Support\Str;
 
 class Product extends Model
 {
+
+    const TYPE_NORMAL = 'normal';
+    const TYPE_CROWDFUNDING = 'crowdfunding';
+    public static $typeMap = [
+        self::TYPE_NORMAL  => '普通商品',
+        self::TYPE_CROWDFUNDING => '众筹商品',
+    ];
+
     protected $fillable = [
         'title',
         'description',
@@ -16,6 +24,7 @@ class Product extends Model
         'sold_count',
         'review_count',
         'price',
+        'type',
     ];
 
     protected $casts    = [
@@ -40,5 +49,9 @@ class Product extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+    public function crowdfunding()
+    {
+        return $this->hasOne(CrowdfundingProduct::class);
     }
 }
