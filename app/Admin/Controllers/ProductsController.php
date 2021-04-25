aa31d5f7cc7d8469269d46e163e1c685bd6aa316
skzhuzhuhappy <15201255789@163.com>
2021-04-25 17:32:22 +0800

    管理后台商品控制器封装

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -9,8 +9,12 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 
-class ProductsController extends AdminController
+class ProductsController extends CommonProductsController
 {
+    public function getProductType()
+    {
+        return Product::TYPE_NORMAL;
+    }
     /**
      * Title for current resource.
      *
@@ -18,12 +22,28 @@ class ProductsController extends AdminController
      */
     protected $title = '商品';
 
-    /**
-     * Make a grid builder.
-     *
-     * @return Grid
-     */
-    protected function grid()
+    protected function customGrid(Grid $grid)
+    {
+        $grid->model()->with(['category']);
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->column('category.name', '类目');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+    }
+
+    protected function customForm(Form $form)
+    {
+        // 普通商品没有额外的字段，因此这里不需要写任何代码
+    }
+
+
+    /*protected function grid()
     {
         $grid = new Grid(new Product);
         // 使用 with 来预加载商品类目数据，减少 SQL 查询
@@ -55,11 +75,6 @@ class ProductsController extends AdminController
         return $grid;
     }
 
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
     protected function form()
     {
         $form = new Form(new Product);
@@ -99,5 +114,6 @@ class ProductsController extends AdminController
         });
 
         return $form;
-    }
+    }*/
+
 }
